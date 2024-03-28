3204468078bcf9cec42f5b96e51d31ca1aa8533a
Tyra Cudjoe <tyra20015@gmail.com>
2024-03-28 09:53:52 +0100

    Add navigation dependencies to App.js file

--- ToDoApp/App.js
+++ ToDoApp/App.js
@@ -8,6 +8,9 @@ import {
     TextInput,
     TouchableOpacity
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Task from './components/Task'
 
 export default function App() {
